28985f5f9dc497a25d09cae3d7a4bbcf5f006530
mrtechreddy <mrtechreddy@gmail.com>
2025-11-03 16:24:17 +0530

    Update outputs.tf

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -1,11 +1,18 @@
-output "instance_ids" {
-  value = [for i in aws_instance.ec2_instance : i.id]
-}
-
-output "public_ips" {
-  value = [for i in aws_instance.ec2_instance : i.public_ip]
-}
-
-output "names" {
-  value = [for i in aws_instance.ec2_instance : i.tags.Name]
+# =========================================================
+# Outputs
+# =========================================================
+output "instances" {
+  value = {
+    for name, instance in aws_instance.ec2_instance :
+    name => {
+      id   = instance.id
+      type = instance.instance_type
+      key  = instance.key_name
+      sgs  = [
+        for sg_key, sg_val in aws_security_group.instance_sg :
+        sg_val.name if sg_key == name
+      ]
+      ip   = instance.public_ip
+    }
+  }
 }
