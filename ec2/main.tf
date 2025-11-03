8b0d2b799e0351b654e907ba2a95e7083d6c8f1a
mrtechreddy <mrtechreddy@gmail.com>
2025-11-03 18:42:38 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,32 +1,43 @@
+#############################################
+# Provider Configuration
+#############################################
 provider "aws" {
   region = var.region
 }
 
-# =========================================================
-# Create security groups dynamically per instance
-# =========================================================
+#############################################
+# Create Dynamic Security Groups
+#############################################
 resource "aws_security_group" "instance_sg" {
   for_each = {
     for instance_name, instance in var.instances :
-    instance_name => flatten(instance.security_groups)
+    instance_name => instance.security_groups
   }
 
-  name_prefix = "${each.key}-sg-"
-  description = "Custom SG for ${each.key}"
+  name_prefix = "${each.key}-"
+  description = "Security group for ${each.key}"
 
+  # Create dynamic ingress rules based on allowed ports
   dynamic "ingress" {
-    for_each = [
-      for sg in each.value :
-      { from_port = sg.allowed_ports[0], to_port = sg.allowed_ports[0], protocol = "tcp" }
-    ]
+    for_each = flatten([
+      for sg in each.value : [
+        for port in sg.allowed_ports : {
+          from_port   = port
+          to_port     = port
+          protocol    = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+      ]
+    ])
     content {
       from_port   = ingress.value.from_port
       to_port     = ingress.value.to_port
       protocol    = ingress.value.protocol
-      cidr_blocks = ["0.0.0.0/0"]
+      cidr_blocks = ingress.value.cidr_blocks
     }
   }
 
+  # Allow all egress
   egress {
     from_port   = 0
     to_port     = 0
@@ -39,9 +50,9 @@ resource "aws_security_group" "instance_sg" {
   }
 }
 
-# =========================================================
-# Create EC2 instances dynamically with corresponding SGs
-# =========================================================
+#############################################
+# Create EC2 Instances
+#############################################
 resource "aws_instance" "ec2_instance" {
   for_each = var.instances
 
@@ -49,7 +60,7 @@ resource "aws_instance" "ec2_instance" {
   instance_type = each.value.instance_type
   key_name      = each.value.key_name
 
-  # Attach all security groups created for this instance
+  # Attach corresponding security group(s)
   vpc_security_group_ids = [
     for sg_key, sg_val in aws_security_group.instance_sg :
     sg_val.id if sg_key == each.key
@@ -57,5 +68,24 @@ resource "aws_instance" "ec2_instance" {
 
   tags = {
     Name = each.key
+  }
+}
+
+#############################################
+# Outputs
+#############################################
+output "ec2_instances" {
+  value = {
+    for name, instance in aws_instance.ec2_instance :
+    name => {
+      instance_id = instance.id
+      type        = instance.instance_type
+      key         = instance.key_name
+      public_ip   = instance.public_ip
+      security_groups = [
+        for sg_key, sg_val in aws_security_group.instance_sg :
+        sg_val.name if sg_key == name
+      ]
+    }
   }
 }
