84f4f23f8eb85b5bb735be006f02c75afc7388e4
mrtechreddy <mrtechreddy@gmail.com>
2025-11-03 16:26:16 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -59,19 +59,3 @@ resource "aws_instance" "ec2_instance" {
     Name = each.key
   }
 }
-
-# =========================================================
-# Outputs
-# =========================================================
-output "instances" {
-  value = {
-    for name, instance in aws_instance.ec2_instance :
-    name => {
-      id   = instance.id
-      type = instance.instance_type
-      key  = instance.key_name
-      sgs  = [for sg in aws_security_group.instance_sg : sg.name if sg_key == name]
-      ip   = instance.public_ip
-    }
-  }
-}
