f3c6f7707f34709610ff04859b9c48fda190c3bc
mrtechreddy <mrtechreddy@gmail.com>
2025-11-03 15:34:26 +0530

    Update variables.tf

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,12 +1,24 @@
-variable "region" {
-  default = "us-east-2"
+variable "instance_name" {
+  type = string
 }
 
 variable "instance_type" {
+  type    = string
   default = "t2.micro"
 }
 
-variable "instance_name" {
-  default = "infragenie-ec2"
+variable "ami" {
+  type    = string
+  default = "ami-0c2b8ca1dad447f8a" # update for your region
 }
 
+variable "key_name" {
+  type        = string
+  description = "Name of the existing AWS key pair to use"
+  default     = null
+}
+
+variable "allowed_ports" {
+  type    = list(number)
+  default = [22]
+}
